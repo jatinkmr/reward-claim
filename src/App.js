20d3c538ffffb3edfa26dec397a14a50d6e5c0b6
Jatin Kamboj <kumar.jatin873@gmail.com>
2025-07-23 17:28:00 +0530

    Update: 404 Page and reward page ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFoundComponent, RewardComponent } from './components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/contest/:contestId' element={<RewardComponent />} />
+          <Route path='*' element={<NotFoundComponent />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
