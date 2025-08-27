e8e9658a2eca6536b06132ca23597a5a9d9bca64
shalurawat-antino <shalu@antino.com>
2025-08-27 12:30:25 +0530

    fix: layout fix and profile section fix

--- src/components/gift/gift.jsx
+++ src/components/gift/gift.jsx
@@ -1,102 +1,148 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { allocatingGiftService } from '../../services';
-import { Container } from 'reactstrap';
-import { GiftAvailAndTermsComponent, DescriptionComponent, ImageSectionComponent, UserHeaderComponent, Loader, PrizeGiftComponent } from '../index';
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { allocatingGiftService } from "../../services";
+import { Container } from "reactstrap";
+import {
+  GiftAvailAndTermsComponent,
+  DescriptionComponent,
+  ImageSectionComponent,
+  UserHeaderComponent,
+  Loader,
+  PrizeGiftComponent,
+} from "../index";
 
 const GiftComponent = () => {
-    const { contestId } = useParams();
-    const [loading, setLoading] = useState(false);
-    const [gift, setGift] = useState({});
-    const navigate = useNavigate();
+  const { contestId } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [gift, setGift] = useState({});
+  const navigate = useNavigate();
 
+  const enrollmentGift = useCallback(async () => {
+    setLoading(true);
 
-    const enrollmentGift = useCallback(async () => {
-        setLoading(true);
+    try {
+      const token = localStorage.getItem("token");
 
-        try {
-            const token = localStorage.getItem('token');
+      const reqBody = { token, contestId };
 
-            const reqBody = { token, contestId };
+      const response = await allocatingGiftService(reqBody);
 
-            const response = await allocatingGiftService(reqBody);
+      if (response?.data?.status.toLowerCase() === "ok") {
+        setGift({
+          giftTitle:
+            response?.data?.data?.gift?.product?.title ||
+            response?.data?.data?.product?.title ||
+            "N/A",
+          giftWorth:
+            response?.data?.data?.gift?.product?.worth ||
+            response?.data?.data?.product?.worth ||
+            "N/A",
+          giftImageUrl:
+            response?.data?.data?.gift?.product?.image[0]?.url ||
+            response?.data?.data?.product?.image[0]?.url ||
+            "",
+          giftDescription:
+            response?.data?.data?.gift?.product?.description ||
+            response?.data?.data?.product?.description ||
+            "N/A",
+          phaseEndDate: response?.data?.data?.phase?.endDate || "N/A",
+          jackPotPrize: response?.data?.data?.phase?.prizeInfo || "N/A",
+          redemptionCode: response?.data?.data?.redemptionCode || "N/A",
+          congratulationHeading:
+            response?.data?.data?.gift?.congratulationHeading ||
+            response?.data?.data?.congratulationHeading ||
+            "Congratulations!",
+          phaseId: response?.data?.data?.phase?.documentId,
+          customerName: response?.data?.data?.customerInfo?.name || "N/A",
+          giftClaimedAt: response?.data?.data?.giftClaimedAt || "N/A",
+        });
+      }
+    } catch (error) {
+      if (
+        error?.response?.data?.code === "MAX_GIFTS_REACHED" &&
+        error?.response?.data?.phase?.documentId
+      ) {
+        let phaseId = error?.response?.data?.phase?.documentId;
+        setGift({
+          phaseEndDate: error?.response?.data?.phase?.endDate || "N/A",
+          jackPotPrize: error?.response?.data?.phase?.prizeInfo || "N/A",
+          phaseId,
+          customerName: error?.response?.data?.customerInfo?.name || "N/A",
+        });
+        navigate(`/contest/${contestId}/phase/${phaseId}`);
+      } else {
+        navigate("/error", {
+          state: {
+            error: {
+              status:
+                error?.response?.status ||
+                error?.response?.data?.error?.status ||
+                500,
+              message:
+                error?.response?.data?.error?.message ||
+                error?.response?.data?.error ||
+                "Unexpected error",
+              details: "Please try again later or contact support.",
+            },
+          },
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [contestId, navigate]);
 
-            if (response?.data?.status.toLowerCase() === "ok") {
-                setGift({
-                    giftTitle: response?.data?.data?.gift?.product?.title || response?.data?.data?.product?.title || 'N/A',
-                    giftWorth: response?.data?.data?.gift?.product?.worth || response?.data?.data?.product?.worth || 'N/A',
-                    giftImageUrl: response?.data?.data?.gift?.product?.image[0]?.url || response?.data?.data?.product?.image[0]?.url || '',
-                    giftDescription: response?.data?.data?.gift?.product?.description || response?.data?.data?.product?.description || 'N/A',
-                    phaseEndDate: response?.data?.data?.phase?.endDate || 'N/A',
-                    jackPotPrize: response?.data?.data?.phase?.prizeInfo || 'N/A',
-                    redemptionCode: response?.data?.data?.redemptionCode || 'N/A',
-                    congratulationHeading: response?.data?.data?.gift?.congratulationHeading || response?.data?.data?.congratulationHeading || 'Congratulations!',
-                    phaseId: response?.data?.data?.phase?.documentId,
-                    customerName: response?.data?.data?.customerInfo?.name || 'N/A',
-                    giftClaimedAt: response?.data?.data?.giftClaimedAt || 'N/A'
-                });
-            }
-        } catch (error) {
-            if (error?.response?.data?.code === "MAX_GIFTS_REACHED" && error?.response?.data?.phase?.documentId) {
-                let phaseId = error?.response?.data?.phase?.documentId;
-                setGift({
-                    phaseEndDate: error?.response?.data?.phase?.endDate || 'N/A',
-                    jackPotPrize: error?.response?.data?.phase?.prizeInfo || 'N/A',
-                    phaseId,
-                    customerName: error?.response?.data?.customerInfo?.name || 'N/A'
-                })
-                navigate(`/contest/${contestId}/phase/${phaseId}`)
-            } else {
-                navigate("/error", {
-                    state: {
-                        error: {
-                            status: error?.response?.status || error?.response?.data?.error?.status || 500,
-                            message: error?.response?.data?.error?.message || error?.response?.data?.error || "Unexpected error",
-                            details: "Please try again later or contact support."
-                        }
-                    }
-                });
-            }
-        } finally {
-            setLoading(false);
-        }
-    }, [contestId, navigate]);
+  useEffect(() => {
+    enrollmentGift();
+  }, [enrollmentGift]);
 
-    useEffect(() => {
-        enrollmentGift();
-    }, [enrollmentGift]);
+  if (loading) return <Loader />;
 
-    if (loading) return <Loader />;
+  return (
+    <Container fluid className="reward-container">
+      <div className="">
+        <UserHeaderComponent customerName={gift.customerName} />
 
-    return (
-        <>
-            <Container fluid className="reward-container">
-                <div className="reward-box">
-                    <UserHeaderComponent customerName={gift.customerName} />
+        <ImageSectionComponent
+          headingText={gift.congratulationHeading}
+          imageUrl={gift.giftImageUrl}
+        />
 
-                    <ImageSectionComponent headingText={gift.congratulationHeading} imageUrl={gift.giftImageUrl} />
+        <p className="won-text">You've won a</p>
 
-                    <p className="won-text">You've won a</p>
+        <DescriptionComponent
+          title={gift.giftTitle}
+          worth={gift.giftWorth}
+          description={gift.giftDescription}
+          isList={false}
+        />
 
-                    <DescriptionComponent title={gift.giftTitle} worth={gift.giftWorth} description={gift.giftDescription} isList={false} />
+        {gift.redemptionCode ? (
+          gift?.giftClaimedAt === "N/A" ? (
+            <p className="redemption-code">
+              Redemption Code: {gift.redemptionCode}
+            </p>
+          ) : (
+            <p className="redemption-code">Claimed</p>
+          )
+        ) : null}
 
-                    {gift.redemptionCode ? (
-                        (gift?.giftClaimedAt === 'N/A') ? (
-                            <p className="redemption-code">
-                                Redemption Code: {gift.redemptionCode}
-                            </p>
-                        ) : (
-                            <p className="redemption-code">Claimed</p>
-                        )
-                    ) : null}
+        {gift?.phaseEndDate !== "N/A" ? (
+          <PrizeGiftComponent
+            contestId={contestId}
+            title="Mega Jackpot"
+            prizeInfo={gift?.jackPotPrize || {}}
+            endDate={gift.phaseEndDate}
+            textBackgroundColor="#4F46E5"
+            imageBackgroundColor="#27218D"
+            phaseId={gift.phaseId}
+          />
+        ) : null}
 
-                    {(gift?.phaseEndDate !== 'N/A') ? <PrizeGiftComponent contestId={contestId} title="Mega Jackpot" prizeInfo={gift?.jackPotPrize || {}} endDate={gift.phaseEndDate} textBackgroundColor="#4F46E5" imageBackgroundColor="#27218D" phaseId={gift.phaseId} /> : null}
-
-                    <GiftAvailAndTermsComponent />
-                </div>
-            </Container>
-        </>
-    )
-}
+        <GiftAvailAndTermsComponent />
+      </div>
+    </Container>
+  );
+};
 
 export default GiftComponent;
