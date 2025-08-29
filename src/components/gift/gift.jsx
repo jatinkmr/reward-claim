60922b44edd47b4fa4d04366836c84f56f644f35
Jatin Kamboj <kumar.jatin873@gmail.com>
2025-08-29 17:43:32 +0530

    fix

--- src/components/gift/gift.jsx
+++ src/components/gift/gift.jsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { allocatingGiftService } from "../../services";
-import { Container } from "reactstrap";
 import {
   GiftAvailAndTermsComponent,
   DescriptionComponent,
